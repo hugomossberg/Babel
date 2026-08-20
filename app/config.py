3a5c9bb5f9e6ee5334327f99db0d3e235dc8eea6
hugo <hugo.mossber1@live.com>
2026-08-20 17:45:54 +0000

    chore: bump version to 0.1.1

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 # All application logic should read from SQLite via get_setting().
 
 APP_NAME = "Babel"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 PORT = int(os.getenv("PORT", "8765"))
 
 # Bug #39: Authentication config
